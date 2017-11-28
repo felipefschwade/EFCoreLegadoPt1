47e704d49df6624c93336ca9474bbaa0e55d267a
Felipe F. Schwade <felipefaeschwade@hotmail.com>
2017-11-27 21:46:04 -0300

    Adicionando connection string do banco para testes.

--- Alura.Filmes.App/Dados/AluraFilmesDbContext.cs
+++ Alura.Filmes.App/Dados/AluraFilmesDbContext.cs
@@ -14,7 +14,8 @@ namespace Alura.Filmes.App.Dados
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Beavis\Documents\AluraFIlmes.mdf;Integrated Security=True;Connect Timeout=30");
+            //optionsBuilder.UseSqlServer(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Beavis\Documents\AluraFIlmes.mdf;Integrated Security=True;Connect Timeout=30");
+            optionsBuilder.UseSqlServer(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Beavis\Documents\AluraFilmesTst.mdf;Integrated Security=True;Connect Timeout=30");
             base.OnConfiguring(optionsBuilder);
         }
     }
