1ff29f94ad4fdf35fdf6ec51d599ef544773776f
Felipe F. Schwade <felipefaeschwade@hotmail.com>
2017-11-27 21:40:49 -0300

    Modificando o nome e os tipos das colunas da tabela actors.

--- Alura.Filmes.App/Negocio/Ator.cs
+++ Alura.Filmes.App/Negocio/Ator.cs
@@ -10,9 +10,11 @@ namespace Alura.Filmes.App.Negocio
     [Table("actor")]
     public class Ator
     {
-        [Column("")]
+        [Column("actor_id")]
         public int Id { get; set; }
+        [Column("first_name", TypeName = "varchar(45)")]
         public string PrimeiroNome { get; set; }
+        [Column("last_name", TypeName = "varchar(45)")]
         public string UltimoNome { get; set; }
     }
 }
