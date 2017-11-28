909236f0a11e087b262f569cdece884b685e9abf
Felipe F. Schwade <felipefaeschwade@hotmail.com>
2017-11-28 19:52:04 -0300

    Adicionando marcações de NotNull

--- Alura.Filmes.App/Negocio/Ator.cs
+++ Alura.Filmes.App/Negocio/Ator.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
@@ -12,8 +13,10 @@ namespace Alura.Filmes.App.Negocio
     {
         [Column("actor_id")]
         public int Id { get; set; }
+        [Required]
         [Column("first_name", TypeName = "varchar(45)")]
         public string PrimeiroNome { get; set; }
+        [Required]
         [Column("last_name", TypeName = "varchar(45)")]
         public string UltimoNome { get; set; }
     }
